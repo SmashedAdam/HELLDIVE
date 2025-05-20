07cf0c4b14aa51d48a64aa1ae78145af3189b574
SmashedAdam <adam.sy.yeung@gmail.com>
2025-05-20 22:47:48 +0200

    added more code

--- src/modules/frontend.py
+++ src/modules/frontend.py
@@ -2,11 +2,12 @@ import tkinter as tk
 import PIL
 import PIL.Image
 
-def hauptBildschrim():
+def titleBildschrim():
     """hauptBildschrim schauen
     """
     root = tk.Tk()
     root.title("Helldivers 2-Ausrüstungshelfer")
     img = PIL.Image.open()
     
-    
+def hauptBildschrim():
+    pass
